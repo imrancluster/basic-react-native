c8049cf0b66270b488fb3599bc36af16cafdc5b9
Imran Sarder <imrancluster@gmail.com>
2019-04-06 21:06:32 +0600

    Fixed text box hidden problem by keyboard

--- src/components/AddReview.js
+++ src/components/AddReview.js
@@ -6,10 +6,19 @@ import {
     View,
     Text,
     TouchableOpacity,
-    StyleSheet
+    StyleSheet,
+    TextInput
 } from 'react-native'
 
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
 export default class AddReview extends Component {
+
+    state = {
+        name: '',
+        rating: 0,
+        review: ''
+    }
 
     close = () => {
         this.props.navigation.goBack()
@@ -17,21 +26,63 @@ export default class AddReview extends Component {
 
   render() {
     return (
-      <View style={styels.root}>
-        <TouchableOpacity 
-        style={styels.button}
-        onPress={this.close}
-        >
-            <Icon name="close" size={30} color="#0066CC" />
-        </TouchableOpacity>
+        <KeyboardAwareScrollView style={{ flex: 1, backgroundColor: '#fff'}}>
+            <View style={styles.root}>
+                <TouchableOpacity 
+                style={styles.button}
+                onPress={this.close}
+                >
+                    <Icon name="close" size={30} color="#0066CC" />
+                </TouchableOpacity>
 
-        <Text tyle={styels.addReview}>Add Review</Text>
-      </View>
+                <Text style={styles.addReview}>Add Review</Text>
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Name (optional)"
+                    value={this.state.name}
+                    onChangeText={name => this.setState({name})}
+                />
+
+                <Text style={styles.rating}>Your Rating: </Text>
+                <View style={styles.stars}>
+                    {
+                        [1,2,3,4,5].map(i => {
+                            return <TouchableOpacity
+                                onPress={() => this.setState({ rating: i })}
+                                style={styles.starButton}
+                                key={i}
+                            >
+                                <Icon 
+                                    name={"star"}
+                                    color={this.state.rating >= i ? "#FFD64C": '#CCCCCC' }
+                                    size={40}
+                                />
+                            </TouchableOpacity>
+                        })
+                    }
+                </View>
+
+                <TextInput 
+                    style={[styles.input, { height: 100 }]}
+                    placeholder="Review"
+                    value={this.state.review}
+                    onChangeText={review => this.setState({ review })}
+                    multiline={true}
+                    numberOfLines={5}
+                />
+
+                <TouchableOpacity style={styles.submitButton}>
+                    <Text style={styles.submitButtonText}>Submit Review</Text>
+                </TouchableOpacity>
+
+            </View>
+        </KeyboardAwareScrollView>
     )
   }
 }
 
-const styels = StyleSheet.create({
+const styles = StyleSheet.create({
     root: {
         flex: 1,
         backgroundColor: '#fff',
@@ -45,5 +96,40 @@ const styels = StyleSheet.create({
         color: '#444',
         textAlign: 'center',
         margin: 20
-    }
+    },
+    input: {
+        padding: 10,
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: "#CCC",
+        borderWidth: 1,
+        borderRadius: 3
+      },
+      rating: {
+        fontSize: 20,
+        color: "grey",
+        textAlign: "center",
+        marginVertical: 40
+      },
+      stars: {
+        marginBottom: 80,
+        flexDirection: "row",
+        justifyContent: "center"
+      },
+      starButton: {
+        padding: 5
+      },
+      submitButton: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: "#0066CC",
+        borderRadius: 4,
+        marginVertical: 10,
+        marginHorizontal: 20
+      },
+      submitText: {
+        fontSize: 18,
+        color: "#FFFFFF",
+        textAlign: "center"
+      }
 })
